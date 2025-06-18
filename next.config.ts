8a4c33eaf92366bc68bc117beef90e194772af99
sarang <saranggp2018@gmail.com>
2025-06-18 17:32:09 +0530

    completed landing page

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,23 @@
 import type { NextConfig } from "next";
+
 const nextConfig: NextConfig = {
-  /* config options here */
-  images : {
-    remotePatterns : [
-     
-    ]
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'utfs.io',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.clerk.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'subdomain.example.com', // Replace with actual domain
+      },
+    ],
   },
-  reactStrictMode : false
 };
 
 export default nextConfig;
