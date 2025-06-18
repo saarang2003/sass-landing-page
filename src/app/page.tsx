c645471abca303739644ba5a5692940c9b6379bd
sarang <saranggp2018@gmail.com>
2025-06-18 15:55:48 +0530

    added navbar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import Footer from "@/components/home/footer";
+import Navbar from "@/components/home/navbar";
 
 export default function Home() {
   return (
     <div >
-       
+       <Navbar/>
       <Footer/>
     </div>
   );
