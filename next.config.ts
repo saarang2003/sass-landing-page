b25ae3b51fccc27095ae169184e3aff30267e807
sarang <saranggp2018@gmail.com>
2025-06-18 14:15:24 +0530

    added shadcn setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
   /* config options here */
+  images : {
+    remotePatterns : [
+     
+    ]
+  },
+  reactStrictMode : false
 };
 
 export default nextConfig;
