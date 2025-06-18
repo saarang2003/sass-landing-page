d4eb4a6aa2ba01a3cfea0457b08864ba80d9491f
sarang <saranggp2018@gmail.com>
2025-06-18 17:10:45 +0530

    added feature section

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -2,6 +2,7 @@ import { Container, Wrapper } from "@/components";
 import { BorderBeam } from "@/components/ui/border-beam";
 import { Button } from "@/components/ui/button";
 import SectionBadge from "@/components/ui/section-badge";
+import { perks } from "@/constants";
 import { ArrowRight, ChevronRight } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
@@ -96,7 +97,48 @@ const HomePage = () => {
                         </p>
                     </div>
                 </Container>
+
+
+                 <Container>
+                    <div className="flex flex-col items-center justify-center py-10 md:py-20 w-full">
+                        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-full divide-x-0 md:divide-x divide-y md:divide-y-0 divide-gray-900 first:border-l-2 lg:first:border-none first:border-gray-900">
+                            {perks.map((perk) => (
+                                <div key={perk.title} className="flex flex-col items-start px-4 md:px-6 lg:px-8 lg:py-6 py-4">
+                                    <div className="flex items-center justify-center">
+                                        <perk.icon className="w-8 h-8" />
+                                    </div>
+                                    <h3 className="text-lg font-medium mt-4">
+                                        {perk.title}
+                                    </h3>
+                                    <p className="text-muted-foreground mt-2 text-start lg:text-start">
+                                        {perk.info}
+                                    </p>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </Container>
          </Wrapper>
+
+
+         {/* features */}
+
+          <Wrapper className="flex flex-col items-center justify-center py-12 relative">
+
+            <div className="hidden md:block absolute top-0 -right-1/3 w-72 h-72 bg-primary rounded-full blur-[10rem] -z-10"></div>
+            <div className="hidden md:block absolute bottom-0 -left-1/3 w-72 h-72 bg-indigo-600 rounded-full blur-[10rem] -z-10"></div>
+            <Container>
+                    <div className="max-w-md mx-auto text-start md:text-center">
+                        <SectionBadge title="Features" />
+                        <h2 className="text-3xl lg:text-4xl font-semibold mt-6">
+                            Discover our powerful features
+                        </h2>
+                        <p className="text-muted-foreground mt-6">
+                            Astra offers a range of features to help you build a stunning website in no time
+                        </p>
+                    </div>
+                </Container>
+          </Wrapper>
     </section>
   );
 };
