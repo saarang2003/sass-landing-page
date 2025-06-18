a18196e92569474a02e1c1d538d64fb77c779140
sarang <saranggp2018@gmail.com>
2025-06-18 16:09:39 +0530

    added ui component

---
+++ src/components/index.ts
@@ -0,0 +1,20 @@
+
+import Providers from "./provider/providers";
+import ThemeProvider from "./provider/theme-providers";
+
+import Navbar from "./home/navbar";
+import Footer from "./home/footer";
+
+import Icons from "./global/icon";
+import Wrapper from "./global/wrapper";
+import Container from "./global/container";
+
+export {
+    Providers,
+    ThemeProvider,
+    Navbar,
+    Icons,
+    Wrapper,
+    Footer,
+    Container,
+}
